4b24afeac6788fe4105f5bcf1e8d15e13cf14752
hexeva <patrick79.g@gmail.com>
2022-01-24 16:57:02 +0100

    end snack 3

--- snack-3/index.php
+++ snack-3/index.php
@@ -7,6 +7,25 @@
     <title>Snack 3</title>
 </head>
 <body>
-    
+<!-- ## Snack 3
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+
+<?php
+// creo un array vuoto
+$random_array = [];
+ 
+// creo un ciclo while che per 15 volte pusherà 15 numeri
+
+while (count($random_array) < 15 ){
+    // per 15 volte genero 15 numeri casuali
+    $random_numbers = rand(1,50);
+    // assgno random_number come key dell'array(ogni key è univoca)
+    $random_array[$random_numbers]= $random_numbers;
+}
+var_dump($random_array);
+
+?>
+
 </body>
 </html>
